45230edfba1fe9735714fe854e2c4c2e1e73a449
Leo Saglia <leosaglia@hotmail.com>
2020-05-03 21:13:13 -0300

    feat(backend): Add appointment repositories(create, all) and the service createAppointService

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,13 +1,36 @@
-import { Router, response } from 'express';
+import { Router } from 'express';
 import { parseISO } from 'date-fns';
 
-const appointmentsRouter = Router();
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
-appointmentsRouter.get('/', (request, response) => {});
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
-  const parsedDate = parseISO(date);
+  try {
+    const { provider, date } = request.body;
+    const parsedDate = parseISO(date);
+
+    const createAppointmentService = new CreateAppointmentService(
+      appointmentsRepository
+    );
+
+    const appointment = createAppointmentService.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
+  }
 });
 
 export default appointmentsRouter;
