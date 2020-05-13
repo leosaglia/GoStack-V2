0d9a9190724f771b4ad077a31a062221df87beb6
Leo Saglia <leosaglia@hotmail.com>
2020-05-12 21:15:12 -0300

    feat(backend): Add create user service

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
+
 import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
-
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
-});
+routes.use('/users', usersRouter);
 
 export default routes;
