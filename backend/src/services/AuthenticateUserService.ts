24db58fc4ddec419090f6bb91f75c71200bdb210
Leo Saglia <leosaglia@hotmail.com>
2020-05-13 20:15:31 -0300

    feat(backend): Add user authentication with JWT

---
+++ backend/src/services/AuthenticateUserService.ts
@@ -0,0 +1,43 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: Request): Promise<Response> {
+    const userRepository = getRepository(User);
+
+    const user = await userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Incorrect email/password combination');
+    }
+
+    const token = sign({}, '83149ada4cf3ae014f97e4ec84d1a8bd', {
+      subject: user.id,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUserService;
