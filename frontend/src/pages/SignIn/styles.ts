6efe4112280f925786a5bd13d51c0a1b3cf66440
Leo Saglia <leosaglia@hotmail.com>
2020-07-01 20:34:32 -0300

    feat(frontend): Add Global style and SignIn status

---
+++ frontend/src/pages/SignIn/styles.ts
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+import signInBackgroundImg from '../../assets/sign-in-background.png';
+
+export const Container = styled.div`
+  height: 100vh;
+
+  display: flex;
+  align-items: stretch;
+`;
+
+export const Content = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  place-content: center;
+
+  width: 100%;
+  max-width: 700px;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    input {
+      background: #232129;
+      width: 100%;
+      padding: 16px;
+
+      border: 2px solid #232129;
+      border-radius: 10px;
+
+      color: #f4ede8;
+
+      &::placeholder {
+        color: #666360;
+      }
+
+      & + input {
+        margin-top: 8px;
+      }
+    }
+
+    button {
+      background: #ff9000;
+
+      height: 56px;
+      width: 100%;
+      padding: 0 16px;
+      margin-top: 16px;
+
+      border-radius: 10px;
+      border: 0;
+
+      color: #312e98;
+      font-weight: 500;
+
+      transition: background-color 0.2s;
+
+      &:hover {
+        background: ${shade(0.2, '#ff9000')};
+      }
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+
+      text-decoration: none;
+
+      transition: color 0.2s;
+
+      & + hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+
+  > a {
+    /* Apenas o 'a' na "raíz" */
+    color: #ff9000;
+    display: flex;
+    align-items: center;
+    margin-top: 24px;
+
+    text-decoration: none;
+
+    transition: color 0.2s;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    & + hover {
+      color: ${shade(0.2, '#ff9000')};
+    }
+  }
+`;
+
+export const Background = styled.aside`
+  flex: 1;
+  background: url(${signInBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
